d3daae08a026a089ffdef85b848835e93fc51203
Martín <martincalderon92@outlook.com>
2024-08-05 20:12:33 -0300

    chore: Update vite.config.ts for testing configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     open: true
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: 'test/vitest.setup.ts',
+    coverage: {
+      include: ['src/**/*']
+    }
   }
 })
