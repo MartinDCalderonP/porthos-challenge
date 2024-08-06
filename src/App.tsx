d1e887c5b511de7f81ff8132c3e5f92a4540c1b6
Martín <martincalderon92@outlook.com>
2024-08-05 21:40:09 -0300

    chore: Add Home page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import styles from './App.module.css'
+import Home from './pages/Home'
 
 const App = () => {
-  return <div className={styles.mainContainer}>App</div>
+  return (
+    <div className={styles.mainContainer}>
+      <Home />
+    </div>
+  )
 }
 
 export default App
