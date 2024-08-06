b32b92591cb9acd9fd0289ca616d3d1dfca0453b
Martín <martincalderon92@outlook.com>
2024-08-05 21:45:03 -0300

    feat: add CitiesList component and related files

---
+++ src/components/CitiesList/cities.ts
@@ -0,0 +1,24 @@
+const cities = [
+  { countryCode: 'AR', city: 'Buenos Aires' },
+  { countryCode: 'BD', city: 'Dhaka' },
+  { countryCode: 'BR', city: 'Brasilia' },
+  { countryCode: 'CA', city: 'Ottawa' },
+  { countryCode: 'CN', city: 'Beijing' },
+  { countryCode: 'FR', city: 'Paris' },
+  { countryCode: 'DE', city: 'Berlin' },
+  { countryCode: 'IN', city: 'New Delhi' },
+  { countryCode: 'IT', city: 'Rome' },
+  { countryCode: 'JP', city: 'Tokyo' },
+  { countryCode: 'MX', city: 'Mexico City' },
+  { countryCode: 'NL', city: 'Amsterdam' },
+  { countryCode: 'RU', city: 'Moscow' },
+  { countryCode: 'ZA', city: 'Pretoria' },
+  { countryCode: 'KR', city: 'Seoul' },
+  { countryCode: 'ES', city: 'Madrid' },
+  { countryCode: 'SE', city: 'Stockholm' },
+  { countryCode: 'CH', city: 'Bern' },
+  { countryCode: 'GB', city: 'London' },
+  { countryCode: 'US', city: 'Washington, D.C. ' }
+]
+
+export default cities
---
+++ src/components/CitiesList/index.tsx
@@ -0,0 +1,31 @@
+import { ChangeEvent } from 'react'
+import styles from './styles.module.css'
+import cities from './cities'
+
+interface CitiesListProps {
+  selectedCity: string
+  setSelectedCity: (city: string) => void
+}
+
+const CitiesList = ({ selectedCity, setSelectedCity }: CitiesListProps) => {
+  const handleCityChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    const value = event.target.value
+    setSelectedCity(value)
+  }
+
+  return (
+    <select
+      className={styles.citiesSelect}
+      value={selectedCity}
+      onChange={handleCityChange}
+    >
+      {cities.map((city) => (
+        <option key={city.city} value={city.city}>
+          {city.city}
+        </option>
+      ))}
+    </select>
+  )
+}
+
+export default CitiesList
