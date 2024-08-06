652e3e34222816b6d0b704c66bffe58cf7df1e5d
Martín <martincalderon92@outlook.com>
2024-08-06 16:45:09 -0300

    refactor: update Home page to display current weather information

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,22 @@
 import CitiesSelect from '../../components/CitiesSelect'
+import CurrentWeather from '../../components/CurrentWeather'
+import useSelectedCity from '../../hooks/useSelectedCity'
+import useFetch from '../../hooks/useFetch'
+import { generateApiUrl } from '../../utils'
+import { Weather } from '../../common/interfaces'
 
 const Home = () => {
+  const { selectedCity } = useSelectedCity()
+  const url = generateApiUrl(selectedCity)
+  const { data, error, loading } = useFetch(url)
+
   return (
     <>
       <h1>Weather App</h1>
       <CitiesSelect />
+      {loading && <div>Loading...</div>}
+      {error && <div>Error: {error.message}</div>}
+      {data && <CurrentWeather weatherData={data as Weather} />}
     </>
   )
 }
