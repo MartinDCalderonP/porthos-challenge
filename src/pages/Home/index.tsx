577ca3018fe16057e9e70c7e1083b51c8cbe4404
Martín <martincalderon92@outlook.com>
2024-08-06 19:58:35 -0300

    refactor: Update Home page to lazy load weather forecast components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -23,11 +23,18 @@ const Home = () => {
       <CitiesSelect />
       {loading && <div>Loading...</div>}
       {error && <div>Error: {error.message}</div>}
+
       {!currentWeatherData && !loading && !error && (
         <div>No data. Please select another city.</div>
       )}
-      {currentWeatherData && <CurrentWeather {...currentWeatherData} />}
-      {nextThreeDays && <ForecastCardsContainer forecastData={nextThreeDays} />}
+
+      {currentWeatherData && !loading && !error && (
+        <CurrentWeather {...currentWeatherData} />
+      )}
+
+      {nextThreeDays && !loading && !error && (
+        <ForecastCardsContainer forecastData={nextThreeDays} />
+      )}
     </>
   )
 }
