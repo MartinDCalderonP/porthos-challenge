7956d2778b9a1c6baf138cc3075a75d781da3f8b
Martín <martincalderon92@outlook.com>
2024-08-06 16:04:53 -0300

    feat: add getDefaultCity utility function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,16 @@
+import { City } from '../common/interfaces'
+
 export const generateGeocodingApiUrl = (selectedCity: string) => {
   if (!selectedCity) return null
 
   return `https://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&appid=${
     import.meta.env.VITE_API_KEY
   }`
+}
+
+export const getDefaultCity = (citiesList: City[]) => {
+  const firstCity = citiesList[0]
+  const defaultCity = `${firstCity.city},${firstCity.countryCode}`
+
+  return defaultCity
 }
