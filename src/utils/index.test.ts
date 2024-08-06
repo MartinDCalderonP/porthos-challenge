3704a1d0c39904af80d8dde40a9d9db4eaf6186b
Martín <martincalderon92@outlook.com>
2024-08-06 20:04:46 -0300

    test: update generateApiUrl to use forecast endpoint instead of weather endpoint

--- src/utils/index.test.ts
+++ src/utils/index.test.ts
@@ -4,7 +4,7 @@ import { describe, test, expect } from 'vitest'
 describe('generateApiUrl', () => {
   test('should generate the correct API URL', () => {
     const selectedCity = 'New York,us'
-    const expectedUrl = `https://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&appid=`
+    const expectedUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${selectedCity}&appid=`
 
     const apiUrl = generateApiUrl(selectedCity)
 
