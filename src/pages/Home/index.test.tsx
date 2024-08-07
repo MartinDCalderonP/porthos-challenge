18a411f21fa9eb7cc3e14c40e56e2b14a958b1ce
Martín <martincalderon92@outlook.com>
2024-08-06 22:44:30 -0300

    test: add unit test for error in Home page

--- src/pages/Home/index.test.tsx
+++ src/pages/Home/index.test.tsx
@@ -1,13 +1,37 @@
-import { render } from '@testing-library/react'
 import Home from './index'
-import { describe, expect, test } from 'vitest'
+import renderWithProviders from '../../utils/renderWithProviders'
+import { describe, expect, test, vi } from 'vitest'
 
 describe('Home', () => {
   test('renders Home component', () => {
-    const { getByText } = render(<Home />)
+    const { getByText } = renderWithProviders(<Home />)
 
     const homeTitle = getByText(/Weather App/i)
 
     expect(homeTitle).toBeInTheDocument()
+  })
+
+  test("renders the loading text when the data hasn't been fetched", () => {
+    const { getByText } = renderWithProviders(<Home />)
+
+    const loadingText = getByText(/Loading/i)
+
+    expect(loadingText).toBeInTheDocument()
+  })
+
+  test('renders the error message when the data fetching fails', async () => {
+    const mockUseFetch = await import('../../hooks/useFetch')
+
+    mockUseFetch.default = vi.fn().mockReturnValueOnce({
+      data: null,
+      error: new Error('An error occurred'),
+      loading: false
+    })
+
+    const { getByText } = renderWithProviders(<Home />)
+
+    const errorText = getByText(/An error occurred/i)
+
+    expect(errorText).toBeInTheDocument()
   })
 })
